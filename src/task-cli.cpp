37e147496c20a68a651d9b70fda847820bf07ec0
ValenteBy <samuel.valente188@gmail.com>
2024-11-09 23:27:51 -0300

    Add basic main

--- src/task-cli.cpp
+++ src/task-cli.cpp
@@ -5,49 +5,76 @@ int main(int argc, char* argv[]){
 
     if(argc < 2)
     {
-        std::cerr << "Use: " << argv[0] << "<comand>" << std::endl;//Apresentar como se deve usar o comando
-        return 1; //retornar erro 
+        std::cerr << "Use: " << argv[0] << "<comand>" << std::endl; // Apresentar como se deve usar o comando
+        return 1; // retornar erro 
     }
     
     if(argc == 2)
     {
         if(argv[2] == "list")
         {
-            //ACESSAR E PRINTAR TODAS AS LISTS SEPARAS
+            // ACESSAR E PRINTAR TODAS AS LISTS SEPARAS
         }
-        else if(argv[2] == "help" || argv[2] == "--h")
+        else if(argv[2] == "--help" || argv[2] == "-h" || argv[2] == "-?")
         {
-            //print help comands
+            // print help comands
+        }
+        else if(argv[2] == "--version" || argv[2] == "-v")
+        {
+            // Printar version
+        }
+        else if(argv[2] == "--author" || argv[2] || "MIAU")
+        {
+            // assinatura
         }
         else
         {
-            //cerr
+            // cerr
         }
     }
 
-    if(argc == 3){
-        if(argv[2] == "list"){
-            if(argv[3] == "todo"){
-                //todo
-            }else if(argv[3] == "done"){
-                //todo
-            }else if(argv[3] == "in-progress"){
-                //todo
-            }else{
-                //cerr
-            }
-        }
-        else if(argv[2] == "update")
+    if((argc == 3) && (argv[3] != "update")){
+        if(argv[2] == "mark-in-progress")
         {
-            ///Stoi(number) try catch 
+            // Recebe ID e modifica
+        }
+        else if(argv[2] == "mark-done")
+        {
+            // Recebe ID e marca como feita
         }
         else if(argv[2] == "delete")
         {
-            //Stoi(number) try catch 
+            // Criar function para tratar entrada mau recebida de inteiro
+        }
+        else if(argv[2] == "list")
+        {
+            if(argv[3] == "done")
+            {
+
+            }
+            if(argv[3] == "todo")
+            {
+
+            }
+            if(argv[3] == "in-progress")
+            {
+
+            }
         }
     }
 
+    if(argc >= 3)
+    {
+        // Criar function para pegar o resto de dos argv
+        if(argv[2] == "add")
+        {
 
+        }
+        else if(argv[2] == "update")
+        {
+            
+        }
+    } 
 
     return 0;
 }
